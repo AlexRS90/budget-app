31d04c82f3228d197db2d4f6bd72745e5bb87fa1
AlexRS90 <aramosbengy@gmail.com>
2022-03-14 14:20:11 -0600

    Generare controllers and views for managment and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'management/index'
+  get 'management/show'
+  get 'categories/index'
+  get 'categories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
 end
