66f0cb6c3b8f50ee872bb7f62af2761927e5f13d
AlexRS90 <aramosbengy@gmail.com>
2022-03-15 17:55:42 -0600

    Refactor FK name

--- db/migrate/20220314203930_create_managments.rb
+++ db/migrate/20220314203930_create_managments.rb
@@ -1,11 +1,13 @@
 class CreateManagments < ActiveRecord::Migration[7.0]
   def change
     create_table :managments do |t|
-      t.references :user, null: false, foreign_key: true
       t.string :name
       t.integer :amount
 
       t.timestamps
     end
+    add_column :managments, :author_id, :bigint
+    add_foreign_key :managments, :users, column: :author_id
+    add_index :managments, :author_id
   end
 end
