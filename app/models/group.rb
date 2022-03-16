6651f4e955b21e0107a2fa55f31732c7e7aaece8
AlexRS90 <aramosbengy@gmail.com>
2022-03-15 22:15:16 -0600

    Fix linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :managements, through: :group_managments
 
-  validates :name, presence: { message: 'can\'t be empty'}
+  validates :name, presence: { message: 'can\'t be empty' }
 end
