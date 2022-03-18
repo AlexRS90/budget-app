3b9146f8df6f1c1c262aab44713d875758f7f420
AlexRS90 <aramosbengy@gmail.com>
2022-03-18 01:02:12 -0600

    Add styling to all views

--- app/models/managment.rb
+++ app/models/managment.rb
@@ -2,4 +2,7 @@ class Managment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_managments
   has_many :groups, through: :group_managments
+
+  validates :name, presence: { message: 'must not be empty' }
+  validates :amount, presence: { message: 'must not be empty' }
 end
